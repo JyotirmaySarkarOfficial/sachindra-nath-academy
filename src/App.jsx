a57a4d90988743853b2198c5e58c2a8909ff0625
JyotirmaySarkarOfficial <jyotirmaysarkarofficial@gmail.com>
2026-05-14 13:56:45 +0530

    Updated Menu Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home'
-import About from './pages/About'
-import Faculty from './pages/Faculty'
-import Gallery from './pages/Gallery'
-import Contact from './pages/Contact'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Faculty from "./pages/Faculty";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
+import SudokuPage from "./pages/SudokuPage";
 
 function App() {
   return (
@@ -14,8 +15,9 @@ function App() {
       <Route path="/faculty" element={<Faculty />} />
       <Route path="/gallery" element={<Gallery />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/sudoku" element={<SudokuPage />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,51 +1,22 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import { BrowserRouter } from 'react-router-dom'
-// import App from './App'
-// import './index.css'
-// import ScrollToTop from './components/ScrollToTop'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <ScrollToTop />
-//       <App />
-//   </BrowserRouter>
-//   </React.StrictMode>,
-// )
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom'
-
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
-import SudokuPage from './pages/SudokuPage'
+import './index.css'
+import ScrollToTop from './components/ScrollToTop'
 
-ReactDOM.createRoot(
-  document.getElementById('root')
-).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-
-        <Route
-          path="/sudoku"
-          element={<SudokuPage />}
-        />
-      </Routes>
+      <ScrollToTop />
+      <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 )
 
 
-
+// npm run dev
 // git add .
 // git commit -m "Updated homepage"
 // git push
